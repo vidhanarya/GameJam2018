d51c4fe7ee802fc7326702d6417b703348fef96d
krishnaw14 <krishnaw14@gmail.com>
2018-03-22 00:58:22 +0530

    few play-alongs
    commting this just becuase getting an error in the local machine

--- Assets/Scripts/PlayerControlScript.cs
+++ Assets/Scripts/PlayerControlScript.cs
@@ -4,66 +4,58 @@ using UnityEngine;
 using UnityEngine.UI;
 public class PlayerControlScript : MonoBehaviour {
 
+	private Rigidbody rb;
+	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
+	public bool onGround;
+	public float jumpSpeed = 5f;
 
-    public bool onGround;
-    private Rigidbody rb;           //player character rigidbody
-    public float jumpSpeed = 5f;         
-    public float speed;             //player motion speed
-    private int count;              //pickup count
-    public Text countText;          //display pickup count
-    public Text winText;            //display winning message
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		onGround = true;
+		winText.text=" ";
+	}
 
-    // Use this for initialization
-    void Start () {
-        rb = GetComponent<Rigidbody>();
-        count = 0;
-        SetCountText();             //updates count text
-        winText.text = " ";
-        onGround = true;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        //Horizontal Movement Mechanic (needs to be replaced)
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        rb.velocity += new Vector3(speed*moveHorizontal, 0.0f, 0.0f);
+	void FixedUpdate() //Before any physics operation
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
 
-        //Jump Mechanic
-        if (onGround)
-        {
-            if (Input.GetKeyDown("space"))
-            {
-                rb.velocity = new Vector3(0f, jumpSpeed, 0f);
-                onGround = false;
-            }
+		if (onGround)
+		{
+			if (Input.GetKeyDown("space"))
+			{
+				rb.velocity = new Vector3(0f,jumpSpeed,0f);
+				onGround = false;
+			}
 
-        }
+		}
 
-    }
-    {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 12)
-        {
-            winText.text = "You Win";
-        }
-    }
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Pick Up"))
-        {
-            other.gameObject.SetActive(false);
-            count = count + 1;
-            SetCountText();
-        }
-    }
+		rb.AddForce (movement*speed);
+	}
 
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.CompareTag("platform"))
-        {
-            onGround = true;
-        }
-    }
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
 
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win";
+		}
+	}
 }
+
