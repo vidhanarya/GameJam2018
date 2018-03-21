9c7547f5ec49fee66f056bb2f09740557d01f5cf
VikramadityaPatil <vikramadityadpatil@outlook.com>
2018-03-21 21:20:50 +0530

    Merge Attempt

---
+++ Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerControlScript : MonoBehaviour {
+
+
+    public bool onGround;
+    private Rigidbody rb;           //player character rigidbody
+    public float jumpSpeed = 5f;         
+    public float speed;             //player motion speed
+    private int count;              //pickup count
+    public Text countText;          //display pickup count
+    public Text winText;            //display winning message
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();             //updates count text
+        winText.text = " ";
+        onGround = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Horizontal Movement Mechanic (needs to be replaced)
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        rb.velocity += new Vector3(speed*moveHorizontal, 0.0f, 0.0f);
+
+        //Jump Mechanic
+        if (onGround)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                rb.velocity = new Vector3(0f, jumpSpeed, 0f);
+                onGround = false;
+            }
+
+        }
+
+    }
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win";
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("platform"))
+        {
+            onGround = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/characterScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterScript : MonoBehaviour {
+    
+    //variables to be logged
+    float hInput, vInput;
+    public bool onGround;
+    private Rigidbody rb;
+    public float jumpSpeed = 5f;
+    public float moveSpeed = 3f;
+    //public Material chMaterial;
+	
+    // Use this for initialization
+	void Start () {
+        print("Yo, I am the character");
+        onGround = true;
+        rb = GetComponent<Rigidbody>();
+        //chMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hInput = Input.GetAxis("Horizontal");
+        //vInput = Input.GetAxis("Vertical");
+        if (onGround)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                rb.velocity = new Vector3(0f,jumpSpeed,0f);
+                onGround = false;
+            }
+
+        }
+        transform.Translate(hInput * Time.deltaTime*moveSpeed, 0f, 0f);
+        //transform.Translate(0f, vInput * Time.deltaTime*moveSpeed, 0f);
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("platform"))
+        {
+            onGround = true;
+        }
+    }
+    
+    //void onDestroy()
+    //{
+    //   Destroy(chMaterial);
+    //}
+}
