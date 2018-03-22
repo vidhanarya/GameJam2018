d2ee2bed1ab157a846cab4ab9340557e9922c7bb
Vidhan <vidhanarya20@gmail.com>
2018-03-22 07:35:34 +0530

    Spawner Added
    Pressing button Q will spawn pickItem in front of player

---
+++ Assets/Scripts/spawnObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnObject : MonoBehaviour {
+
+
+    public GameObject spawnItem;
+    public GameObject player;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GameObject clone;
+			clone = Instantiate(spawnItem, new Vector3(transform.position.x - 0.4f, transform.position.y + 0.144814f, transform.position.z), Quaternion.identity);
+        }
+    }
+}
